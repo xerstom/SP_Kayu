8380428033609e934a1a11a60d3ef61e4e80be30
Alexandre BOUVARD <albouvard@local.isima.fr>
2022-01-11 19:09:38 +0100

    🚀Init

---
+++ src/main/java/com/boulec/kayu/KayuApplication.java
@@ -0,0 +1,13 @@
+package com.boulec.kayu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KayuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KayuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/boulec/kayu/KayuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boulec.kayu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KayuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
